97cfaf58229a797f61f97abfd98b286cc76cebef
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2010-03-16 17:35:40 +0100

    Añadido aviso de licencia

--- hola_mundo.c
+++ hola_mundo.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Hola Mundo de siempre
+// Copyright (C) 2010 Antonio García Domínguez
+// Programa bajo la licencia GPLv3
+
 int main() {
   printf("¡Hola mundo!\n");
   return EXIT_SUCCESS;
