e8c98a2b662408b826259efe9a5b20ff7bfb32fd
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2010-03-17 09:07:56 +0100

    Añadido el cálculo de términos de Fibonacci

--- hola_mundo.c
+++ hola_mundo.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fib(int n) {
+  int a = 0, b = 1;
+  while (n > 0) {
+    int c = a + b;
+    a = b;
+    b = c;
+    --n;
+  }
+  return a;
+}
+
 int main() {
+  int n;
   printf("¡Hola mundo!\n");
-  printf("Añado una línea.\n");
+
+  printf("Introduce un número: ");
+  scanf("%d", &n);
+  printf("fib(%d) = %d\n", n, fib(n));
+
   return EXIT_SUCCESS;
 }
